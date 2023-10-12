421f48eeafad2d33f732a4aa83c825b3003e3811
MHNazary <mhnazary2020@gmail.com>
2023-10-12 20:25:30 +0430

    Added fizzbuzz metho to solver.rb

--- solver.rb
+++ solver.rb
@@ -8,4 +8,15 @@ class Solver
     end
     return num * factorial(num - 1)
   end
+  def fizzbuzz(number)
+      if (number % 3).zero? && (number % 5).zero?
+        'fizzbuzz'
+      elsif (number % 3).zero?
+        'fizz'
+      elsif (number % 5).zero?
+        'buzz'
+      else
+        number
+      end
+    end
 end
