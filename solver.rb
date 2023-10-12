ed04ccab77fbdf911f4d183927e884a3f36983ea
SnakyMz <Soban.syeed@outlook.com>
2023-10-12 21:31:51 +0530

    Add reverse method to solver class

--- solver.rb
+++ solver.rb
@@ -8,15 +8,20 @@ class Solver
     end
     return num * factorial(num - 1)
   end
+
+  def reverse(word)
+    word.reverse
+  end
+  
   def fizzbuzz(number)
-      if (number % 3).zero? && (number % 5).zero?
-        'fizzbuzz'
-      elsif (number % 3).zero?
-        'fizz'
-      elsif (number % 5).zero?
-        'buzz'
-      else
-        number
-      end
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number
     end
+  end
 end
