fb2f872786a738b5e86a672f817ef5ab207e12c4
SnakyMz <Soban.syeed@outlook.com>
2023-10-12 20:51:20 +0530

    Add test for reverse method

--- solver_spec.rb
+++ solver_spec.rb
@@ -1,10 +1,11 @@
 require 'solver'
 
 describe Solver do
-  context "When Testing the factorial method " do
-    before(:each) do 
-      @solver = Solver.new()
-    end
+  before(:each) do 
+    @solver = Solver.new()
+  end
+
+  context "When Testing the factorial method " do  
     it "return 1 when the argument is 0" do
       expect(@solver.factorial(0)).to eq(1)
     end
@@ -13,6 +14,18 @@ describe Solver do
     end
     it "Raise exception when the argument is negative" do
       expect(@solver.factorial(-1)).to raise_exception('not factorial by negative numbers')
+    end
+  end
+
+  context "When testing the reverse method" do
+    it "return 'olleh' when the argument is 'hello'" do
+      expect(@solver.reverse('hello')).to eql('olleh')
+    end
+    it "return 'h' when the argument is 'h'" do
+      expect(@solver.reverse('h')).to eql('h')
+    end
+    it "return 'A1' when the argument is '1A'" do
+      expect(@solver.reverse('A1')).to eql('1A')
     end
   end
 end
